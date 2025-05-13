e4992e1d81c54bb4841061b988b6055bd2088a06
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-05-13 08:24:34 +0530

    Create swap_the_numbers.py

---
+++ Difficulty: School/Swap The Numbers/swap_the_numbers.py
@@ -0,0 +1,17 @@
+#{
+  # Driver Code Starts
+# } Driver Code Ends
+
+a = int(input())
+b = int(input())
+
+########### Write your code below ###############
+# Write Code to Swap
+a, b = b, a
+########### Write your code above ###############
+
+print(a, b)
+
+#{
+  # Driver Code Starts
+# } Driver Code Ends
