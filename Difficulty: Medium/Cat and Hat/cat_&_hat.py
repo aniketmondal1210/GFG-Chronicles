571089b800b1dba7f66b4d55ef4589470c8537ee
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-05-14 08:47:45 +0530

    Create cat_&_hat.py

---
+++ Difficulty: Medium/Cat and Hat/cat_&_hat.py
@@ -0,0 +1,17 @@
+#{
+  # Driver Code Starts
+# } Driver Code Ends
+
+#User function Template for python3
+
+def cat_hat(str):
+  ##your code here##
+  ##You need to write complete code this time 
+    if str.count("cat") == str.count("hat"):
+        return True
+    else:
+        return False
+
+#{
+  # Driver Code Starts
+# } Driver Code Ends
