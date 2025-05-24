b35a6f023401c0c0b3efd3a70093c645cbb6eaf6
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-05-24 11:07:11 +0530

    Update check_the_status.py

--- Difficulty: Easy/Check the status - Python/check_the_status.py
+++ Difficulty: Easy/Check the status - Python/check_the_status.py
@@ -1,7 +1,12 @@
 class Solution:
     def checkStatus(self, a, b, flag):
         # code here
-        if flag:
-            return a < 0 and b < 0
+        if (a < 0 and b < 0) and flag == True:
+            return True
+        elif (a > 0 and b > 0):
+            return False
+        elif (a > 0 or b > 0) and flag == False:
+            return True
         else:
-            return (a >= 0 and b < 0) or (a <= 0 and b > 0)
+            return False
+            
