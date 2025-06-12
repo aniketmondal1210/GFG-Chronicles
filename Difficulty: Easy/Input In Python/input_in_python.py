2343bf5df59f928cde2b9abccd712cb0435cc21b
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-06-12 07:57:32 +0530

    Create input_in_python.py

---
+++ Difficulty: Easy/Input In Python/input_in_python.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+########### Write your code below ###############
+
+# Take string input and print the string input
+a = input()
+print(a)
+# Take integer input and add 10 to the integer input and print
+b = int(input())
+print(b+10)
+# Take floating-point input and multiply the float input by 10 and print
+c = float(input())
+print(c*10)
+########### Write your code above ###############
