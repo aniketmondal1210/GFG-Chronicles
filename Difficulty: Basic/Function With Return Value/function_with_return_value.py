dca1358c601310ed0206b14654e6e800877b52d6
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-06-20 07:57:52 +0530

    Create function_with_return_value.py

---
+++ Difficulty: Basic/Function With Return Value/function_with_return_value.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+
+#Write the complete function returnValueFunction below
+#This function should take n as a parameter
+#Return 2*n
+#code here
+def returnValueFunction(n:int):
+    return 2*n
