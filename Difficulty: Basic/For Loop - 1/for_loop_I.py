97d46d3eca29add11a1bb4b10753a19a25a95ed7
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-06-23 09:10:57 +0530

    Update for_loop_I.py

--- Difficulty: Basic/For Loop - 1/for_loop_I.py
+++ Difficulty: Basic/For Loop - 1/for_loop_I.py
@@ -3,3 +3,16 @@ n = int(input())
 for i in range(1,11):
     print(n*i,end = " ")
 # Your code here
+
+
+
+OR
+
+
+
+#User function Template for python3
+def utility():
+    n = int(input())
+    ## in is a membership operator that is true if something is a member of sequence
+    for i in range(1,11):  ## i in range(x,y,z) means i goes from x to y-1 and increments z steps in each iteration
+        print(i * n, end=" ")  ## Separating by spaces using end =" "
