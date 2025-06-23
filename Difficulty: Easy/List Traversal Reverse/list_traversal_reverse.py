2a64352de75bbe451568195a2909142c4f918e25
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-06-23 08:54:40 +0530

    Create list_traversal_reverse.py

---
+++ Difficulty: Easy/List Traversal Reverse/list_traversal_reverse.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+def listTraversalReverse(arr):
+    #Write your code below
+    arr = arr[::-1]
+    for i in arr:
+        print(i,end=" ")
+    #Don't add a new line as it is provided by the driver code
