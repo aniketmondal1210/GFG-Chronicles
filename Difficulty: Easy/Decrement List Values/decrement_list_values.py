10979f8f92d699df9fbd1ebd69e96043b9149aea
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-06-25 08:36:52 +0530

    Create decrement_list_values.py

---
+++ Difficulty: Easy/Decrement List Values/decrement_list_values.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+def decrementList(arr):
+    #Write the code below to decrement the numbers by 1
+    ##After decrementing return the list
+    #Don't print it here
+    result = []
+    for i in arr:
+        result.append(i-1)
+    return result
