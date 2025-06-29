e88928540865843c212545a892b2b3073885669d
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-06-29 08:22:36 +0530

    Create append_to_list.py

---
+++ Difficulty: Basic/Append To List/append_to_list.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+
+def appendToList(a,b,c):
+    #Write the code below
+    #Create a list
+    result = []
+    #Append a b c in order
+    result.append(a)
+    result.append(b)
+    result.append(c)
+    #Return the list
+    return result
+    #Dont print here
+    
+
+    
