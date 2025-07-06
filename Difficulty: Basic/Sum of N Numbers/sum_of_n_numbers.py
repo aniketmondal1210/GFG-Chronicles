03d330b6f2a494e1ae6c62d175f05ff40fa6a27e
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-07-06 08:02:32 +0530

    Update sum_of_n_numbers.py

--- Difficulty: Basic/Sum of N Numbers/sum_of_n_numbers.py
+++ Difficulty: Basic/Sum of N Numbers/sum_of_n_numbers.py
@@ -5,3 +5,12 @@ class Solution:
         for i in range(1,n+1):
             sum += i
         return sum
+
+
+OR
+
+
+class Solution:
+    def sumOfNumbers(self, n):
+        # code here
+        return n*(n+1)//2
