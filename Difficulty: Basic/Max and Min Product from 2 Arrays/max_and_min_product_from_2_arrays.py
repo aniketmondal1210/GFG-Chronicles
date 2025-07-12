60bee3c40233b7b6f2bd43d2e4271ce6342be822
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-07-12 12:02:50 +0530

    Create max_and_min_product_from_2_arrays.py

---
+++ Difficulty: Basic/Max and Min Product from 2 Arrays/max_and_min_product_from_2_arrays.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+class Solution:
+    # Function to find the maximum element from array arr1 and 
+    # the minimum element from array arr2 and return their product.
+    def find_multiplication(self, arr1, arr2):
+        # code here
+        return max(arr1)*min(arr2)
