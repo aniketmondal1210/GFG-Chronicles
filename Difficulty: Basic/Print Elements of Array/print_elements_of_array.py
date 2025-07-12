c9ec64231641fa1e0b714e4ac391fa362e06cb47
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-07-12 11:51:27 +0530

    Create print_elements_of_array.py

---
+++ Difficulty: Basic/Print Elements of Array/print_elements_of_array.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+class Solution:
+    # Just print the space seperated array elements
+	def printArray(self, arr):
+	    # code here
+	    for i in arr:
+	        print(i,end=" ")
