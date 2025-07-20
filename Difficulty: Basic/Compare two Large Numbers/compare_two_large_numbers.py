efd79063b12f6e57bc3a19656a7bd58f4896481b
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-07-20 10:58:31 +0530

    Create compare_two_large_numbers.py

---
+++ Difficulty: Basic/Compare two Large Numbers/compare_two_large_numbers.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+class Solution:
+    def check(self, a,b):
+        # code here
+        if int(a) < int(b):
+            return 1
+        elif int(a) > int(b):
+            return 2
+        else:
+            return 3
