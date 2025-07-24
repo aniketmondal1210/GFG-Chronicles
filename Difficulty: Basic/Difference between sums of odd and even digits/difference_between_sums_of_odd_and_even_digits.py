e5ba93de25023ff156921bb4ca490baa56710b79
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-07-24 10:15:33 +0530

    Create difference_between_sums_of_odd_and_even_digits.py

---
+++ Difficulty: Basic/Difference between sums of odd and even digits/difference_between_sums_of_odd_and_even_digits.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+class Solution:
+    def oddAndEven(self, S):
+        odd, even = 0, 0
+        for i in range(len(S)):
+            if i % 2 == 0:
+                even += int(S[i])
+            else:
+                odd += int(S[i])
+        return 1 if even == odd else 0
