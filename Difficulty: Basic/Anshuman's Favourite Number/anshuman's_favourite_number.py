930cfe7773ec036286ec362a643a39df2529c38b
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-07-25 08:43:33 +0530

    Create anshuman's_favourite_number.py

---
+++ Difficulty: Basic/Anshuman's Favourite Number/anshuman's_favourite_number.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+class Solution:
+    def isValid (self,N):
+        # code here 
+        if N % 5 == 0:
+            return "YES"
+        else:
+            return "NO"
