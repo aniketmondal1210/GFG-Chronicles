e978e87462682de71e6bfe5636f0b148948e9079
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-07-28 08:30:36 +0530

    Delete Difficulty: Easy/Divisibility by 999 directory

--- Difficulty: Easy/Divisibility by 999/divisibility_by_999.py
+++
@@ -1,8 +0,0 @@
-#User function Template for python3
-class Solution:
-    def isDivisible999(self, N):
-        # code here
-        if int(N) % 999 == 0:
-            return "Divisible"
-        else:
-            return "Not divisible"
