7abed41859ff2d318958b021bcb6573973798fff
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-07-30 09:25:51 +0530

    Create mean_and_median_of_array.py

---
+++ Difficulty: Easy/Mean And Median of Array/mean_and_median_of_array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def median(self, arr):
+        #code here
+        arr.sort()
+        if len(arr) % 2 == 0:
+            return (arr[len(arr)//2] + arr[(len(arr)//2)-1])//2
+        else:
+            return arr[len(arr)//2]
+    
+    def mean(self, arr):
+        #code here
+        return sum(arr)//len(arr)
