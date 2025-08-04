c8ad54ce27f579ad62176061a523aaf0f68c1642
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-04 09:30:41 +0530

    Create input_integer.java

---
+++ Difficulty: Basic/Input Integer/input_integer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class GFG {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        Scanner s = new Scanner(System.in);
+        //  Take input
+        int a = s.nextInt();
+        //  Add 10 and Print
+        System.out.println(a+10);
+    }
+}
