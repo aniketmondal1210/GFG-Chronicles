c70c3c4da02f2b5104464004db55ec29e27b730c
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-06 09:33:03 +0530

    Create sum_of_first_n_natural_numbers.java

---
+++ Difficulty: Easy/Sum of First N Natural Numbers/sum_of_first_n_natural_numbers.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+class Solution {
+    static int sumOfNaturals(int n) {
+        // code here
+        if(n == 0){
+            return 0;
+        }
+        else{
+            return (n*(n+1))/2;
+        }
+    }
+};
