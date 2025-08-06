aa39ffa273ff1a0f7c748a3c35fb5158cf42fce3
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-06 09:39:58 +0530

    Create input_string.java

---
+++ Difficulty: Basic/Input String/input_string.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Take String Input and Print it
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+    }
+}
