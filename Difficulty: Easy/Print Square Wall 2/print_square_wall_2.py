bea1a01c730dd579e958816e8b9f228de2ed57bd
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-07 08:59:05 +0530

    Create print_square_wall_2.py

---
+++ Difficulty: Easy/Print Square Wall 2/print_square_wall_2.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+def squareWall(s):
+    #Complete the codes below 
+    #Replace .... with your own code
+    for i in range(1,s+1):
+        
+        for j in range(1,s+1):
+            print("*",end=" ")
+        #This print below gives new line, so you don't have to.
+        print()
