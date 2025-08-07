932d18670949d4acaa8f91639cafef15d2d16644
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-07 08:59:21 +0530

    Update print_square_wall_2.py

--- Difficulty: Easy/Print Square Wall 2/print_square_wall_2.py
+++ Difficulty: Easy/Print Square Wall 2/print_square_wall_2.py
@@ -3,7 +3,6 @@ def squareWall(s):
     #Complete the codes below 
     #Replace .... with your own code
     for i in range(1,s+1):
-        
         for j in range(1,s+1):
             print("*",end=" ")
         #This print below gives new line, so you don't have to.
