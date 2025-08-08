09df1e83e00ca370ebe3bd131d63487205612443
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-08 11:37:06 +0530

    Create unique_number_iii.py

---
+++ Difficulty: Medium/Unique Number III/unique_number_iii.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+from collections import Counter
+class Solution:
+    def getSingle(self, arr):
+        # code here 
+        a = Counter(arr)
+        for key,value in a.items():
+            if value == 1:
+                return key
