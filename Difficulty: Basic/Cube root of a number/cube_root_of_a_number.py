134e7c2dcb4ea0f337fa42b9a6bd0f5007e6a264
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-10 11:14:52 +0530

    Update cube_root_of_a_number.py

--- Difficulty: Basic/Cube root of a number/cube_root_of_a_number.py
+++ Difficulty: Basic/Cube root of a number/cube_root_of_a_number.py
@@ -4,3 +4,17 @@ class Solution:
     def cubeRoot(self, n):
         # code here 
         return int(math.pow(n,1/3))
+
+
+
+
+OR 
+
+
+
+#User function Template for python3
+import math
+class Solution:
+    def cubeRoot(self, n):
+        # code here 
+        return math.floor(math.pow(n,1/3))
