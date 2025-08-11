9596329e5e92e4e0322fc3f1e4b3ad209945ae5d
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-11 11:07:06 +0530

    Delete Difficulty: Basic/Sum of product of x and y with floor(n\\x) = y directory

--- Difficulty: Basic/Sum of product of x and y with floor(n\\x) = y/sum_of_product_of_x_and_y_with_floor(n\\x)_=_y.py
+++
@@ -1,9 +0,0 @@
-#User function Template for python3
-import math
-class Solution:
-	def sumofproduct(self, n):
-		# Code here
-		result = 0
-		for i in range(1,n+1):
-		    result += i*math.floor(n/i)
-        return result
