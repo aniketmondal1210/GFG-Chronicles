43b7f5295bb7f40e65a41ad1f568bc984de78a4c
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-20 11:00:31 +0530

    Create doubling_the_value.py

---
+++ Difficulty: Basic/Doubling the value/doubling_the_value.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+class Solution:
+    def solve(self,n : int, a : list, b : int):
+        # Complete this function
+        while(b in arr):
+            b *= 2
+        return b
