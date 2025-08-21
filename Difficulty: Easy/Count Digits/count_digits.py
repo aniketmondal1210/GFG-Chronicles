125b3d831ae323ea66d586177756ff7cfc7a5706
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-21 22:21:45 +0530

    Create count_digits.py

---
+++ Difficulty: Easy/Count Digits/count_digits.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+class Solution:
+    def evenlyDivides(self, n):
+        # code here
+        count = 0
+        for i in str(n):
+            if i != '0' and n % int(i) == 0:
+                count += 1
+        return count
