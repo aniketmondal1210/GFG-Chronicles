008e612753fcb1318de74d9e4bb3c6ae67682eaf
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-22 09:34:23 +0530

    Create c++_operators(relational)_|_set_2.java

---
+++ Difficulty: Basic/C++ Operators (Relational) | Set 2/c++_operators(relational)_|_set_2.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+class Solution {
+    static String compareNum(int A, int B) {
+        if(A>B){
+            return A +" is greater than " + B;
+        }
+        else if(A<B){
+            return A + " is less than " + B;
+        }
+        else{
+            return A + " is equals to " + B;
+        }
+    }
+}
