7c746b1dd91ec4c70552240a07446f93b0ede690
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-23 11:18:59 +0530

    Update gcd_of_two_numbers.py

--- Difficulty: Basic/GCD of two numbers/gcd_of_two_numbers.py
+++ Difficulty: Basic/GCD of two numbers/gcd_of_two_numbers.py
@@ -1,5 +1,15 @@
+class Solution:
+    def gcd(self, a, b):
+        # code here
+        while b != 0:
+            a,b = b, a % b
+        return a
+
+
+'''
+***Dont' Use***
 import math
 class Solution:
     def gcd(self, a, b):
         # code here
-        return math.gcd(a,b)
+        return math.gcd(a,b)'''
