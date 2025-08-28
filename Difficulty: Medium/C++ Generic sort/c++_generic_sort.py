7c320a7e1ab468697510f7ef1af79fd1cd2ff549
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-08-28 17:41:09 +0530

    Create c++_generic_sort.py

---
+++ Difficulty: Medium/C++ Generic sort/c++_generic_sort.py
@@ -0,0 +1,16 @@
+#include <algorithm> // For std::sort
+#include <iostream>
+using namespace std;
+
+template <class T>
+void sortArray(T a[], int n) {
+    sort(a, a + n);
+}
+
+template <class T>
+void printArray(T a[], int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
