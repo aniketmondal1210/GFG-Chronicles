dfbf2b2f9f39e6fbc5021c106fca546844b0a7dc
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-01 22:25:49 +0530

    Create c++_data_types.cpp

---
+++ Difficulty: Basic/C++ Data Types/c++_data_types.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int cppIntType() {
+        int n;
+        cin >> n;
+        return n;
+    }
+    
+    char cppCharType() {
+        char ch;
+        cin >> ch;
+        return ch;
+    }
+    
+    float cppFloatType() {
+        float f;
+        cin >> f;
+        return f;
+    }
+};
