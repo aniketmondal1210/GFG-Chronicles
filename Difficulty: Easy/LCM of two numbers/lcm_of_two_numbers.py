0b46bda8888562025770673ea5ad53537bcd3a46
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-02 11:05:26 +0530

    Update lcm_of_two_numbers.py

--- Difficulty: Easy/LCM of two numbers/lcm_of_two_numbers.py
+++ Difficulty: Easy/LCM of two numbers/lcm_of_two_numbers.py
@@ -3,3 +3,22 @@ class Solution:
     def lcm(self, a, b):
         # code here
         return math.lcm(a,b)
+
+
+OR 
+
+
+class Solution:
+    def hcf(self, a, b):
+        if a == 0:
+            return abs(b)
+        if b == 0:
+            return abs(a)
+        while b:
+            a, b = b, a % b
+        return abs(a)
+    
+    def lcm(self, a, b):
+        if a == 0 or b == 0:
+            return 0
+        return abs(a * b) // self.hcf(a, b)
