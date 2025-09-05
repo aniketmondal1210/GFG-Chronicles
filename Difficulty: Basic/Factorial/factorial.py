c3e3a6f089811737e4ddf3af4494f3352185b0d8
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-05 19:55:58 +0530

    Update factorial.py

--- Difficulty: Basic/Factorial/factorial.py
+++ Difficulty: Basic/Factorial/factorial.py
@@ -1,7 +1,8 @@
-#User function Template for python3
-n = int(input())
-# Your code here
-prod = 1
-for i in range(1,n+1):
-    prod *= i
-print(prod)
+class Solution:
+    # Function to calculate factorial of a number.
+    def factorial(self, n: int) -> int:
+        # code here
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
