894e2514d977ccff68cba21b203bdcc5e8a3a5a1
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-07 19:18:40 +0530

    Create minimum_number_in_a_sorted_rotated_array.py

---
+++ Difficulty: Easy/Minimum Number in a sorted rotated array/minimum_number_in_a_sorted_rotated_array.py
@@ -0,0 +1,6 @@
+#User function Template for python3
+class Solution:
+    #Function to find the minimum element in sorted and rotated array.
+    def minNumber(self, arr,low,high):
+        #Your code here
+        return min(arr)
