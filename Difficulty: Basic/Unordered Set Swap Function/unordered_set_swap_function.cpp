e74bc6987dd13018a94181c9b004190d39600fb3
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-14 10:25:04 +0530

    Create unordered_set_swap_function.cpp

---
+++ Difficulty: Basic/Unordered Set Swap Function/unordered_set_swap_function.cpp
@@ -0,0 +1,8 @@
+// User function Template for C++
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+void SwapMe(unordered_set<string> &first, unordered_set<string> &second) {
+    first.swap(second);
+}
