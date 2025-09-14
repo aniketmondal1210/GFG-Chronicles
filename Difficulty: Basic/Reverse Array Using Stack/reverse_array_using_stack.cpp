d408656e993c55188b59eb858ef19db12258b7d6
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-14 10:32:33 +0530

    Create reverse_array_using_stack.cpp

---
+++ Difficulty: Basic/Reverse Array Using Stack/reverse_array_using_stack.cpp
@@ -0,0 +1,12 @@
+// User function Template for C++
+class Solution {
+  public:
+    void reverseArray(vector<int>& arr) {
+        int left = 0, right = arr.size() - 1;
+        while (left < right) {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+};
