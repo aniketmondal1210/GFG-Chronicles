f5435d785c1655012b03084c2e63b773bb34f405
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-15 19:43:11 +0530

    Create find_element_in_set_using_count.cpp

---
+++ Difficulty: Easy/Find element in Set using Count/find_element_in_set_using_count.cpp
@@ -0,0 +1,7 @@
+// User function Template for C++
+
+// Complete this function
+bool sExists(set<int>& s, int x) {
+    // find returns iterator to element if found, else s.end()
+    return s.find(x) != s.end();
+}
