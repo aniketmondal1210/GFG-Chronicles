f85e3f318bdc0ef3f3a536e90b01c36b71118336
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-17 15:40:05 +0530

    Create sort_and_reverse.cpp

---
+++ Difficulty: Basic/Sort and reverse/sort_and_reverse.cpp
@@ -0,0 +1,6 @@
+// User function Template for C++
+
+void sortAndReverse(vector<int> &arr) {
+    sort(arr.begin(), arr.end());     // Sort in ascending order
+    reverse(arr.begin(), arr.end());  // Reverse to get descending order
+}
