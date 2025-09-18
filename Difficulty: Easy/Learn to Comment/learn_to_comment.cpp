b4b81a0382e4ebf89658269ebe1f7acbd4959d77
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-18 22:30:56 +0530

    Create learn_to_comment.cpp

---
+++ Difficulty: Easy/Learn to Comment/learn_to_comment.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    void printValues(int a, int b, int c) {
+        // code here
+        
+        cout << a << " ";
+        // cout << b << " ";  // This line is commented out to skip printing b
+        cout << c << " ";
+    }
+};
