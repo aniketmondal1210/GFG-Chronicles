e1243057b2abaec959b04e860bc2e44af59ac68f
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-22 18:00:41 +0530

    Create swap_the_object-double_pointers.cpp

---
+++ Difficulty: Easy/Swap the objects-Double pointers/swap_the_object-double_pointers.cpp
@@ -0,0 +1,8 @@
+// User function Template for C++
+
+// Function to swap two objects **obj1 and **obj2
+void swapper(Person **obj1, Person **obj2) {
+    Person *temp = *obj1;
+    *obj1 = *obj2;
+    *obj2 = temp;
+}
