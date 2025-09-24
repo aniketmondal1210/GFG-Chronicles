f8259e66d163a4a4d033c1d2e791903d7a6ee2b1
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-24 22:21:58 +0530

    Create find_volume.cpp

---
+++ Difficulty: Easy/Find Volume/find_volume.cpp
@@ -0,0 +1,13 @@
+// User function Template for C++
+
+// Write the first function to
+// return the volume of cube
+// Function to return the volume of a cube
+int volume(int s) {
+    return s * s * s;  // Volume of cube = side^3
+}
+
+// Function to return the volume of a rectangular box
+int volume(int l, int w, int h) {
+    return l * w * h;  // Volume of rectangular box = length * width * height
+}
