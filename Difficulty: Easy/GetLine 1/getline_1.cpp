44d1fa2e9cee3fb8a1239de946028af0904b1898
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-25 15:35:21 +0530

    Create getline_1.cpp

---
+++ Difficulty: Easy/GetLine 1/getline_1.cpp
@@ -0,0 +1,11 @@
+// User function Template for C++
+
+// Function to take input of string using getline
+void getLine() {
+    string s;
+
+    // Take input of 's' using getline
+    getline(cin, s);
+
+    cout << s << endl;
+}
