550b055e0e581c3663ed9de5b841f54498858fc8
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-29 11:53:35 +0530

    Create vector_sum.cpp

---
+++ Difficulty: Basic/Vector Sum/vector_sum.cpp
@@ -0,0 +1,11 @@
+// User function Template for C++
+
+// Complete the function
+#include <bits/stdc++.h>
+using namespace std;
+
+// Complete the function
+int sumMe(vector<int> arr, int n) {
+    // accumulate(start, end, initial_value)
+    return accumulate(arr.begin(), arr.end(), 0);
+}
