9ef85ca325459daf85dae3fd5668028b0bbe2bc6
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-09-29 11:57:16 +0530

    Create increasing_roll_and_marks.cpp

---
+++ Difficulty: Easy/Increasing Roll And Marks/increasing_roll_and_marks.cpp
@@ -0,0 +1,7 @@
+// User function Template for C++
+
+vector<pair<int, pair<int, int>>> increasing_List(vector<pair<int, pair<int, int>>> v) {
+    // Sort using default comparator (lexicographical order)
+    sort(v.begin(), v.end());
+    return v;
+}
