5466dd2906d439d3c11a5e21808583070c13ba10
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-10-12 10:46:31 +0530

    Create move_all_negative_elements_to_end.py

---
+++ Difficulty: Easy/Move all negative elements to end/move_all_negative_elements_to_end.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+class Solution:
+    def segregateElements(self, arr):
+        # Your code goes here
+        pos = []
+        neg = []
+        for i in arr:
+            if i >= 0:
+                pos.append(i)
+            else:
+                neg.append(i)
+
+        arr[:] = pos + neg
