a4e3488b44dfa3c4d8e5b9fa95e4a21b7bd80c2f
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-10-17 10:36:36 +0530

    Create the_and_gate.py

---
+++ Difficulty: Basic/The AND Gate/the_and_gate.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+class Solution:
+    def andGate (self, arr, N):
+        # code here
+        result = 1
+        for i in arr:
+            if i == 0:
+                return 0
+        return result
