c5eef8f69072035eb39d0f9f59a9a35c746f6cf4
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-10-31 11:54:33 +0530

    Create numbers_containing_0's_from_1_to_N.py

---
+++ Difficulty: Basic/Numbers containing 0's from 1 to N/numbers_containing_0's_from_1_to_N.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+class Solution:
+	def CountNo(self, N):
+		# Code here
+		count = 0
+        for i in range(1,n + 1):
+            if '0' in str(i):
+                count += 1
+        return count 
