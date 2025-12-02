4b75ccd44b2b1f98f4682a927f56c58af104efb3
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-12-02 23:34:36 +0530

    Create two_dimentional_world.cpp

---
+++ Difficulty: Easy/Two Dimensional World/two_dimentional_world.cpp
@@ -0,0 +1,14 @@
+/* Function to take input for 2D array elements
+ * Note : Mention matrix in argument also.
+ * N : size of matrix
+ */
+void twoDimensional(int** arr, int N) {
+    // Loop to iterate through matrix
+    // and print all the matrix elements row wise
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
