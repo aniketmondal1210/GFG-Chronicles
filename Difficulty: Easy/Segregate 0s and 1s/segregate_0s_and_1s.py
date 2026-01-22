bbc2c3088438e918ca2254ff69cd3a69674c8062
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-01-22 20:54:11 +0530

    Create segregate_0s_and_1s.py

---
+++ Difficulty: Easy/Segregate 0s and 1s/segregate_0s_and_1s.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+class Solution:
+    def segregate0and1(self, arr):
+        # code here
+        a = arr.count(0)
+        for i in range(a):
+            arr[i] = 0
+        for j in range(a,len(arr)):
+            arr[j] = 1
+        return arr
