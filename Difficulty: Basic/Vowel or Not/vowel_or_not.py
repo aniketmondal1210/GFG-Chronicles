9cc8ee11a5dfceb552c4c9d7a31f9461b6dc07dc
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-02-13 14:03:25 +0530

    Create vowel_or_not.py

---
+++ Difficulty: Basic/Vowel or Not/vowel_or_not.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+class Solution:
+    def isVowel (ob,c):
+        # code here 
+        vowels = "aeiouAEIOU"
+        if c in vowels:
+            return "YES"
+        else:
+            return "NO"
