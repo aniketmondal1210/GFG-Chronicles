70054dfca6f478a6849df145d482dc7fda9141d7
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-02-14 10:56:11 +0530

    Create nth_fibonacci_number.py

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth_fibonacci_number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nthFibonacci(self, n: int) -> int:
+        # code here
+        if n == 0:
+            return 0
+        a = 0
+        b = 1
+        for i in range(n-1):
+            c = a + b
+            a,b = b,c
+        return b
