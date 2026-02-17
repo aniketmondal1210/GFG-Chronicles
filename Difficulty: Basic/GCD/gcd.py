e3b9f908b2ef9941c0bffc5b04ee9605ef4dd0e0
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-02-17 09:29:08 +0530

    Update gcd.py

--- Difficulty: Basic/GCD/gcd.py
+++ Difficulty: Basic/GCD/gcd.py
@@ -12,3 +12,13 @@ else:
         if a%i == 0 and b%i==0:
             result.append(i)
     print(max(result))
+
+
+OR
+
+
+#User function Template for python3
+import math
+def gcd(a, b):
+    # code here to calculate and return gcd of a and b
+    return math.gcd(a,b)
