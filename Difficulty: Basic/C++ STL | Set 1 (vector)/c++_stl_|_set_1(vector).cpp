b62bf761fc0e11be84bb755188f01f7d25e040da
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-02-25 09:37:07 +0530

    Create c++_stl_|_set_1(vector).cpp

---
+++ Difficulty: Basic/C++ STL | Set 1 (vector)/c++_stl_|_set_1(vector).cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/* inserts an element x at the back of the vector A */
+void add_to_vector(vector<int> &A, int x) {
+    A.push_back(x);
+}
+
+/* sorts the vector A in ascending order */
+void sort_vector_asc(vector<int> &A) {
+    sort(A.begin(), A.end());
+}
+
+/* reverses the vector A */
+void reverse_vector(vector<int> &A) {
+    reverse(A.begin(), A.end());
+}
+
+/* returns the size of the vector A */
+int size_of_vector(vector<int> &A) {
+    return A.size();
+}
+
+/* sorts the vector A in descending order */
+void sort_vector_desc(vector<int> &A) {
+    sort(A.begin(), A.end(), greater<int>());
+}
+
+/* prints space separated values of the vector */
+void print_vector(vector<int> &A) {
+    for (int i = 0; i < A.size(); i++) {
+        cout << A[i] << " ";
+    }
+}
