35f88cadf273f0080dd3ce11c87ee5c5719e8e46
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-03-25 12:39:13 +0530

    Implement length of linked list function

---
+++ Difficulty: Basic/Length of Linked List/length_of_linked_list.py
@@ -0,0 +1,14 @@
+'''
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+'''
+class Solution:
+    def getCount(self, head):
+        # code here
+        count = 0
+        while head:
+            count += 1
+            head = head.next
+        return count
