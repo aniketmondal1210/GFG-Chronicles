ea814d168023e028ef0da76969be70d41685be83
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-03-28 10:41:54 +0530

    Create search_in_linked_list.py

---
+++ Difficulty: Basic/Search In Linked List/search_in_linked_list.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def searchLinkedList(self, head, x):
+        #code here
+        current = head
+        while current:
+            if current.data == x:
+                return 1
+            current = current.next
+        return 0
