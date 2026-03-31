e3ad9fab9d99e97c63c095fb4203893a351631cf
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-03-31 10:23:34 +0530

    Add searchKey method to find key in linked list
    Implement search functionality in linked list.

---
+++ Difficulty: Easy/Search in Linked List/search_in_linked_list.py
@@ -0,0 +1,15 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def searchKey(self, head, key):
+        #Code here
+        current = head
+        while current:
+            if current.data == key:
+                return True
+            current = current.next
+        return False
