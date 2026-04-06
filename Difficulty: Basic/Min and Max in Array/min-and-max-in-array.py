a44f8836520913b8d20573df2eb6eb0e7b7187a3
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-04-06 14:18:30 +0530

    Refactor get_min_max to getMinMax and optimize logic
    Refactor get_min_max method to getMinMax for better clarity and performance. Implement logic to find minimum and maximum values without sorting.

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.py
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.py
@@ -1,31 +1,11 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
-
-#User function Template for python3
-#User function Template for python3
-
 class Solution:
-    def get_min_max(self, arr):
-        arr.sort()
-        return [arr[0],arr[-1]]
-
-
-#{ 
- # Driver Code Starts.
-#Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
-        arr = list(map(int, input().split()))
-        ob = Solution()
-        mn, mx = ob.get_min_max(arr)
-        print(mn, mx)
-        t -= 1
-        print("~")
-
-
-# } Driver Code Ends
+    def getMinMax(self, arr):
+        # code here
+        mini = float('inf')
+        maxi = float('-inf')
+        for i in arr:
+            if i < mini:
+                mini = i
+            if i > maxi:
+                maxi = i
+        return [mini, maxi]
