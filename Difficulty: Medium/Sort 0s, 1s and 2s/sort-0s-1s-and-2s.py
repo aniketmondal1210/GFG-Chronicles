267fd21b6d60b796a1a9c4ec2c9586638f95f496
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-04-08 20:44:21 +0530

    Update sort-0s-1s-and-2s.py

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -1,7 +1,17 @@
 class Solution:
-    # Function to sort an array of 0s, 1s, and 2s
     def sort012(self, arr):
         # code here
-        arr.sort()
+        count0 = arr.count(0)
+        count1 = arr.count(1)
+        count2 = arr.count(2)
+        idx = 0
+        for i in range(count0):
+            arr[idx] = 0
+            idx += 1
+        for i in range(count1):
+            arr[idx] = 1
+            idx += 1
+        for i in range(count2):
+            arr[idx] = 2
+            idx += 1
         return arr
-        
