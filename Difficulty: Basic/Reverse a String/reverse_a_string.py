6ac00eb503b44eec6726caac594fafd2e6a6b5bd
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-04-23 11:39:13 +0530

    Update reverse_a_string.py

--- Difficulty: Basic/Reverse a String/reverse_a_string.py
+++ Difficulty: Basic/Reverse a String/reverse_a_string.py
@@ -2,4 +2,10 @@
 class Solution:
      def reverseString(self, s: str) -> str:
         # code here
-        return s[::-1]
+        a = list(s)
+        left,right = 0,len(s)-1
+        while(left<right):
+            a[left],a[right] = a[right],a[left]
+            left += 1
+            right -= 1
+        return "".join(a)
