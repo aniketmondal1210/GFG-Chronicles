7b2dedfefa5dcf7bb503eec8949cc78dde48c67f
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-05-11 14:15:13 +0530

    Create peak_element.py

---
+++ Difficulty: Medium/Peak element/peak_element.py
@@ -0,0 +1,14 @@
+class Solution:   
+    def peakElement(self, arr):
+        # Code here
+        n = len(arr)
+        if n == 1:
+            return 0
+        if arr[0] > arr[1]:
+            return 0
+        if arr[n-1] > arr[n-2]:
+            return n-1
+        for i in range(1, n - 1):
+            if arr[i-1] < arr[i] and arr[i] > arr[i+1]:
+                return i
+        return -1
