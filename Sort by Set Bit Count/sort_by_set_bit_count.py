25d0ab5b7a339b07e9e05078a8e489b68292f488
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-05-22 09:49:33 +0530

    Delete Sort by Set Bit Count directory

--- Sort by Set Bit Count/sort_by_set_bit_count.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def sortBySetBitCount(self, arr):
-        # code here
-        def count_set_bits(n):
-            return bin(n).count('1')
-        arr.sort(key=lambda x: -count_set_bits(x))
-        return arr
