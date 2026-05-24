04dd24e19c843b283a924a13725b57dd0444fdec
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-05-25 00:55:41 +0530

    Create occurrences_of_2_as_a_digit.py

---
+++ Difficulty: Medium/Occurences of 2 as a digit/occurrences_of_2_as_a_digit.py
@@ -0,0 +1,10 @@
+def number0f2s(n):
+    #add Code here
+        count = 0
+        for i in range(n + 1):
+            count += str(i).count('2')
+        return count
+    
+def numberOf2sinRange(n):
+    #add code here
+    return number0f2s(n)
