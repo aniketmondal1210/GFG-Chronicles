1fd313867a79a7c539e0ec0db4487276f64d93b6
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-05-24 10:47:12 +0530

    Create sum_of_squares_of_first_n_natiral_numbers.py

---
+++ Difficulty: Easy/Sum of Squares of First n Natural Numbers/sum_of_squares_of_first_n_natiral_numbers.py
@@ -0,0 +1,6 @@
+#User function Template for python3
+class Solution:
+    # Function to calculate the sum of squares of first 'number' natural numbers
+    def sumOfSquares(self, number):
+        # code here
+        return (number*(number+1)*(2*number+1))//6
