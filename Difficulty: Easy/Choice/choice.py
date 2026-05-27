300d12a8a572423159672a6648ee44dbd9ca3ab3
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-05-27 17:05:58 +0530

    Create choice.py

---
+++ Difficulty: Easy/Choice/choice.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+class Solution:
+    def arrayForm(self, a, b, c):
+        # Code here
+        return [a,b,c]
+    def stringForm(self, a, b, c):
+        # Code here
+        return str(a)+str(b)+str(c)
