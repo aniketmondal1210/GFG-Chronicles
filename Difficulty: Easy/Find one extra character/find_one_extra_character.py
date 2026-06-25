66f298889f6c24de0951e075307dd3cb6308b088
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-06-25 11:16:36 +0530

    Update find_one_extra_character.py

--- Difficulty: Easy/Find one extra character/find_one_extra_character.py
+++ Difficulty: Easy/Find one extra character/find_one_extra_character.py
@@ -1,10 +1,9 @@
-from collections import Counter
 class Solution: 
     def extraChar(self,s1, s2):
         # code here
-        a = Counter(s1)
-        b = Counter(s2)
-        for i in b:
-            if b[i] != a[i]:
-                return i
-        return ''
+        result = 0
+        for i in s1:
+            result ^= ord(i)
+        for j in s2:
+            result ^= ord(j)
+        return chr(result)
