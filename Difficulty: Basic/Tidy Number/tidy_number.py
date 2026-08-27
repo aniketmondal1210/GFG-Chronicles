1d18203dba86a8bcad297b39ca67844f7458bc74
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2026-08-27 11:35:30 +0530

    Add isTidy method to Solution class
    Implement isTidy method to check if a number is tidy.

--- Difficulty: Basic/Tidy Number/tidy_number.py
+++ Difficulty: Basic/Tidy Number/tidy_number.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def isTidy(self, n: int) -> bool:
+        # code here
+        s = str(n)
+        for i in range(1, len(s)):
+            if s[i] < s[i-1]:
+                return False
+        return True
